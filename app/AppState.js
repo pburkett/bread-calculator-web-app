89c177ea7ddd2b18a65b84537251a3caa8abac47
pburkett <pburkett636@gmail.com>
2020-12-26 19:09:07 -0700

    removed all default 'value' related files

--- app/AppState.js
+++ app/AppState.js
@@ -1,12 +1,10 @@
-// import Value from "./Models/Value.js"
+
 import Formula from "./Models/Formula.js"
 import DoughWeight from "./Models/DoughShape.js"
 import { EventEmitter } from "./Utils/EventEmitter.js"
 import { isValidProp } from "./Utils/isValidProp.js"
 
 class AppState extends EventEmitter {
-  // /** @type {Value[]} */
-  // values = []
 
   formulas = []
   doughShapes = []
